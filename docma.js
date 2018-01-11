d0fded0ce864add74f564347a9b0184da4503b02
陆书明 <lucien-lu@qq.com>
2018-01-11 15:56:02 +0800

    create docs by docma

---
+++ docma.js
@@ -0,0 +1,61 @@
+const docma = require('docma');
+
+const config = {
+    "src": [
+        "./om.js"
+    ],
+    "dest": "./docs",
+    template: {
+        // Template-specific options
+        options: {
+            title: "OpenMap Library",
+            sidebar: true,
+            collapsed: false,
+            badges: true,
+            search: true,
+            toolbar: true,
+            symbolMeta: true,
+            outline: "tree",
+            animations: true,
+            navbar: true,
+            navItems: [
+                {
+                    label: "Documentation",
+                    href: "#",
+                    iconClass: "ico-book"
+                },
+                {
+                    label: "Demos",
+                    href: "#",
+                    iconClass: "ico-mouse-pointer"
+                },
+                {
+                    label: "Download",
+                    iconClass: "ico-md ico-download",
+                    items: [
+                        {
+                            label: "Source Code",
+                            href: "git@github.com:lucienlugeek/om.git"
+                        },
+                        {
+                            label: "Zip",
+                            href: "https://codeload.github.com/lucienlugeek/om/zip/lite"
+                        }
+                    ]
+                },
+                {
+                    label: "GitHub",
+                    href: "https://github.com/lucienlugeek/om",
+                    target: "_blank",
+                    iconClass: "ico-md ico-github"
+                }
+            ]
+        }
+    }
+};
+
+docma.create()
+    .build(config)
+    .catch(err => {
+        console.log(err);
+    })
